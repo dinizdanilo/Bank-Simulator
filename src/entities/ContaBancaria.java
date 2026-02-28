c08b7c455c149317f31dc35768f0a6c63931dbe6
Danilo Diniz <dgdevbr@gmail.com>
2026-02-28 16:57:26 -0300

    feature/construtores e métodos para depósito

--- src/entities/ContaBancaria.java
+++ src/entities/ContaBancaria.java
@@ -1,22 +1,44 @@
 package entities;
 
 public class ContaBancaria {
-    private int numeroConta;
+
+    private static final double TAXA_SAQUE = 5.00;
+
+    private final int numeroConta;
     private String titular;
     private double saldo;
-
-    public void deposito(double valor) {
-        this.saldo += valor;
-    }
-
-    public ContaBancaria(int numeroConta, String titular, double depositoInicial) {
-        this.numeroConta = numeroConta;
-        this.titular = titular;
-        deposito(depositoInicial);
-    }
 
     public ContaBancaria(int numeroConta, String titular) {
         this.numeroConta = numeroConta;
         this.titular = titular;
     }
+    public ContaBancaria(int numeroConta, String titular, double depositoInicial) {
+        this(numeroConta, titular);
+        if (depositoInicial > 0) {
+            deposito(depositoInicial);
+        }
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void deposito(double valor) {
+         if (valor > 0.0) {
+             this.saldo += valor;
+         }
+    }
+
 }
