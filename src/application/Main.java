b64454bb98d18ee9b93239228b7759631d647048
Danilo Diniz <dgdevbr@gmail.com>
2026-03-01 00:31:33 -0300

    feature/aplicando laço de repetição para depósitos adicionais

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,17 +19,34 @@ public class Main {
         String titular = sc.nextLine();
 
         System.out.print("Deseja fazer um depósito inicial (s/n)? ");
-        String depositoInicial = sc.nextLine().trim().toLowerCase();
+        String deposito = sc.nextLine().trim().toLowerCase();
 
         ContaBancaria conta;
-        if (depositoInicial.equals("s")) {
+        if (deposito.equals("s")) {
             System.out.print("Digite o valor de depósito inicial: ");
             double valor = sc.nextDouble();
+            sc.nextLine();
             conta = new ContaBancaria(numeroConta, titular, valor);
         }
         else {
             conta = new ContaBancaria(numeroConta, titular);
         }
+
+        System.out.print(conta + "\n");
+
+        do {
+            System.out.print("\nDeseja fazer um depósito adicional (s/n)? ");
+            deposito = sc.nextLine().trim().toLowerCase();
+            if (deposito.equals("n")) {
+                break;
+            }
+
+            System.out.print("\nDigite o valor do depósito: ");
+            double valor = sc.nextDouble();
+            sc.nextLine();
+            conta.deposito(valor);
+
+        } while (deposito.equals("s"));
 
         System.out.print(conta);
 
